f5fe8be4adccc8c110aa56d04fd74149aa1f8df7
Romanov Andre <romanovandre05@gmail.com>
2021-07-04 22:40:37 -0400

    Spring Math: Calculate with Querystrings

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -1,14 +1,34 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
+@RequestMapping("/math")
 public class MathController {
 
-    @GetMapping("/math/pi")
+    @GetMapping("/pi")
     public String getMathPI() {
         return String.valueOf(Math.PI);
     }
 
+    @GetMapping("/calculate")
+    public int getCalculate(@RequestParam(required = false, defaultValue = "sum") String operation, int x, int y) {
+        if (operation.equals("sum"))return x + y;
+        if (operation.equals("subtract"))return x - y;
+        if (operation.equals("multiply"))return x * y;
+        if (operation.equals("divide"))return x / y;
+        return 0;
+    }
+
+    @PostMapping("/sum")
+    public int sum(@RequestParam int... n) {
+        return Arrays.stream(n).sum();
+    }
+
+
+
 }
+
+
--- src/test/java/com/example/demo/MathControllerTest.java
+++ src/test/java/com/example/demo/MathControllerTest.java
@@ -28,4 +28,25 @@ public class MathControllerTest {
                 .andExpect(content().string(String.valueOf(Math.PI)));
     }
 
+    @Test
+    public void testMathCalculate() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=sum&x=15&y=3");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.valueOf(18)));
+    }
+
+    @Test
+    public void testSum() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.post("/math/sum?n=5&n=3&n=2");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.valueOf(10)));
+    }
+
+
+
+
+
+
 }
